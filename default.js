980cd0eae1e4b0ff6e0a70c1646f2505cb38e8c2
Brandon Bruno <brandon.bruno@outlook.com>
2022-05-17 08:38:33 -0400

    Created structure for default.js.

--- default.js
+++ default.js
@@ -1,14 +1,34 @@
-let tech = document.getElementsByClassName("years-active")
-let thisYear = new Date().getFullYear();
+(function () {
 
-for (var i = 0; i < tech.length; i++) {
+	Site = window.Site || {
 
-	let year = tech[i].attributes.getNamedItem("data-year");
-	
-	if (year != null) {
-	
-		tech[i].innerHTML = `${(thisYear - year.value)} years`;
-	
-	}
+		init: function () {
 
-}
+			Site.transformYearsActive();
+
+		},
+
+		transformYearsActive: function () {
+
+			let tech = document.getElementsByClassName("years-active")
+			let thisYear = new Date().getFullYear();
+			
+			for (var i = 0; i < tech.length; i++) {
+			
+				let year = tech[i].attributes.getNamedItem("data-year");
+				
+				if (year != null) {
+				
+					tech[i].innerHTML = `${(thisYear - year.value)} years`;
+				
+				}
+			}
+		}
+
+	};
+
+	// Entry point
+	init();
+
+})();
+
